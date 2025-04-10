9a1d30e72e793f85aa00b09e29e149e7290b6b55
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-04-11 00:03:20 +0300

    Remove background stripes feature and JavaScript files; update header styles

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
   import('./js/burgerMenu.js');
-  import('./js/backgroundStripes.js').then(module => {
-    const initBackgroundStripes = module.default;
-    initBackgroundStripes();
-  });
 });
